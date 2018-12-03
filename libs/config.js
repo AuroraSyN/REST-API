2ef95412f59f005068d3753f39a170cafb5e374a
Aleksandr Soloninov <etj.sad@gmail.com>
2018-12-03 13:30:48 +0100

    Simple AOuth

--- libs/config.js
+++ libs/config.js
@@ -1,7 +1,15 @@
 var nconf = require('nconf');
-//
-nconf.argv()
-    .env()
-    .file({ file: './config.json' });
+
+nconf.argv().env();
+
+if (process.env.ENV_IN === 'docker') {
+    nconf.file('docker', {
+        file: process.cwd() + '/docker.config.json'
+    });
+}
+
+nconf.file('defaults', {
+    file: process.cwd() + '/config.json'
+});
 
 module.exports = nconf;
